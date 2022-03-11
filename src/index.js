fc012408f1d0de30e0b1fa6a04f0d1aff4fa22dd
Neil Kollack <nkollack@gmail.com>
2022-03-10 21:04:24 -0600

    feat: implement wrapping up 3

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                key={`square${i}`}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
             />
@@ -21,25 +22,15 @@ class Board extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+        var innerDivs = [];
+        for (var i = 0; i < 3; i++) {
+            var rows = [];
+            for (var j = 0; j < 3; j++) {
+                rows.push(this.renderSquare(3 * i + j));
+            }
+            innerDivs.push(<div className="board-row" key={`board-row${i}`}>{rows}</div>);
+        }
+        return <div>{innerDivs}</div>;
     }
 }
 
